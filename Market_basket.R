2a8224dd5cac71f14e1aef1a5407e3ea84a3482e
Vivek Singh <35369075+singhvivekrbl@users.noreply.github.com>
2018-05-05 09:57:50 +0530

    Update Market_basket.R

--- Market_basket.R
+++ Market_basket.R
@@ -12,7 +12,7 @@ itemFrequencyPlot(Groceries,topN=20,type="absolute")
 # Get the rules
 rules <- apriori(Groceries, parameter = list(supp = 0.001, conf = 0.8))
 
-# Show the top 5 rules, but only 2 digits
+## Show the top 5 rules, but only 2 digits
 options(digits=2)
 inspect(rules[1:5])
 rules<-sort(rules, by="confidence", decreasing=TRUE)
